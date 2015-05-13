2b73608852812ed8621f74b0c152e91101219fd3
Dino Cikatic <dcikatic@edx.org>
2015-05-13 15:59:25 +0200

    SOL-487 Implement LmsSearchInitializer

---
+++ lms/lib/courseware_search/lms_search_initializer.py
@@ -0,0 +1,24 @@
+"""
+This file contains implementation override of SearchInitializer which will allow
+    * To set initial set of masquerades and other parameters
+"""
+
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
+
+from search.initializer import SearchInitializer
+from courseware.masquerade import setup_masquerade
+from courseware.access import has_access, get_user_role
+
+class LmsSearchInitializer(SearchInitializer):
+    """ SearchInitializer for LMS Search """
+    def initialize(self, **kwargs):
+        if 'request' in kwargs and kwargs['request'] and kwargs['course_id']:
+            request = kwargs['request']
+            try:
+                course_key = CourseKey.from_string(kwargs['course_id'])
+            except InvalidKeyError:
+                course_key = SlashSeparatedCourseKey.from_deprecated_string(kwargs['course_id'])
+            staff_access = has_access(request.user, 'staff', course_key)
+            masquerade = setup_masquerade(request, course_key, staff_access)
